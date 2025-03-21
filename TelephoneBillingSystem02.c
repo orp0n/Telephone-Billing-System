86b2cbba8fda0c6f17cd2d49e58a7f3278410b22
Md. Ashikuzzaman Ashik <ashikuzzaman241-35-253@diu.edu.bd>
2025-03-21 09:38:47 +0600

    Update TelephoneBillingSystem02.c

--- TelephoneBillingSystem02.c
+++ TelephoneBillingSystem02.c
@@ -1,52 +1,70 @@
 #include <stdio.h>
 #include <string.h>
 
-struct userInfo {
+typedef struct usersInfo {
     char username[50];
     char password[50];
-} ;
+}USER;
 
-struct userInfo registeredUser;
+# define max_users 100
+USER registeredUser[max_users];
+int user_count=0;
 
+//UDF for Registration Functionality
 void registerUser() {
     FILE *file = fopen("User Information.txt", "w");
 
     printf("\n| --- Register --- |\n");
     printf("Enter username: ");
-    scanf("%s", registeredUser.username);
+    scanf("%s",&registeredUser[user_count].username);
     printf("Enter password: ");
-    scanf("%s", registeredUser.password);
-    printf("Registration successful!\n");
+    scanf("%s",&registeredUser[user_count].password);
+    
+    user_count++;
+    printf("Registration Successful!\n");
 
-    fprintf(file, "Username: %s\n", registeredUser.username);
-    fprintf(file, "Password: %s\n", registeredUser.password);
+    printf("user count = %d",user_count);
+
+
+    fprintf(file, "Username: %s\n", registeredUser[user_count].username);
+    fprintf(file, "Password: %s\n", registeredUser[user_count].password);
     fclose(file);
 }
 
+//UDF for Log in Functionality
 void loginUser() {
+
     char username[50], password[50];
     printf("\n| --- Login --- |\n");
     printf("Enter username: ");
-    scanf("%s", username);
+    scanf("%s",username);
     printf("Enter password: ");
-    scanf("%s", password);
+    scanf("%s",password);
 
-    if (strcmp(username, registeredUser.username) == 0 && strcmp(password, registeredUser.password) == 0) {
-        printf("Login successful. Welcome, %s!\n", username);
-    } else {
-        printf("Invalid username or password.\n");
+    int found=0;
+    for(int i=0;i<user_count;i++){
+        if (strcmp(username, registeredUser[i].username) == 0 && strcmp(password, registeredUser[i].password) == 0) {
+            printf("Login successful. Welcome, %s!\n", username);
+            found=1;
+            break;
+        }
+        if(found!=1) {
+            printf("Invalid username or password.\n");
+        }
     }
+
 }
 
 int main() {
     int choice;
 
     while (1) {
+        //Navigation Menu
         printf("\n| --- Menu --- |\n");
         printf("1. Register\n");
         printf("2. Login\n");
         printf("3. Exit\n");
-        printf("Enter your choice: ");
+        printf("Select from Navigation Menu: ");
         scanf("%d", &choice);
 
         switch (choice) {
