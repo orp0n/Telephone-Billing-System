3406079aefaced73601e454ca6014d17a57bd94b
Md. Ashikuzzaman Ashik <ashikuzzaman241-35-253@diu.edu.bd>
2025-03-22 10:51:05 +0600

    Update TelephoneBillingSystem02.c

--- TelephoneBillingSystem02.c
+++ TelephoneBillingSystem02.c
@@ -1,64 +1,108 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include<conio.h>
 
-typedef struct usersInfo {
+struct userInfo {
     char username[50];
+    char email[50];
     char password[50];
-}USER;
+} user;
 
-# define max_users 100
-USER registeredUser[max_users];
-int user_count=0;
+struct userInfo registeredUser;
+
+//UDF for Password validation Functionality
+int isPasswordValid(char *password) {
+    int hasUpper = 0, hasLower = 0, hasDigit = 0, hasSpecial = 0;
+    for (int i = 0; password[i]; i++) {
+        if (isupper(password[i])) 
+            hasUpper = 1;
+        else if (islower(password[i])) 
+            hasLower = 1;
+        else if (isdigit(password[i])) 
+            hasDigit = 1;
+        else 
+            hasSpecial = 1;
+    }
+    return hasUpper && hasLower && hasDigit && hasSpecial && strlen(password) >= 8;
+}
+
+//UDF for Set Password Functionality
+void maskPassword(char *password) {
+    char ch;
+    int i = 0;
+    
+    printf("Enter password: ");
+    while ((ch = getch())!= '\r') {
+        if (ch == '\b' && i > 0) {
+            printf("\b \b");
+            i--;
+        } else if (isprint(ch) && i < 49) {
+            password[i++] = ch;
+            printf("*");
+        }
+    }
+    password[i] = '\0';
+}
 
 //UDF for Registration Functionality
 void registerUser() {
-    FILE *file = fopen("User Information.txt", "a");
-
+    FILE *file = fopen("UserInformation.txt", "a");
+    
     printf("\n| --- Register --- |\n");
     printf("Enter username: ");
-    scanf("%s",&registeredUser[user_count].username);
-    printf("Enter password: ");
-    scanf("%s",&registeredUser[user_count].password);
+    scanf("%s", user.username);
+    printf("Enter email: ");
+    scanf("%s", user.email);
     
-    user_count++;
-    printf("Registration Successful!\n");
+    do {
+        maskPassword(user.password);
+        if (!isPasswordValid(user.password)) {
+            printf("\nPassword must be at least 8 characters with upper/lowercase, number, and special character.\n");
+        }
+    } while (!isPasswordValid(user.password));
 
-    printf("user count = %d",user_count);
-
-
-    fprintf(file, "Username: %s\n", registeredUser[user_count].username);
-    fprintf(file, "Password: %s\n", registeredUser[user_count].password);
+    fprintf(file, "Email: %s\nPassword: %s\n", user.email, user.password);
     fclose(file);
+
+    printf("\nRegistration successful!\n");
 }
 
 //UDF for Log in Functionality
 void loginUser() {
+    char email[50], password[50];
+    int found = 0;
 
-    char username[50], password[50];
     printf("\n| --- Login --- |\n");
-    printf("Enter username: ");
-    scanf("%s",username);
-    printf("Enter password: ");
-    scanf("%s",password);
+    printf("Enter email: ");
+    scanf("%s", email);
+    maskPassword(password);
 
-    int found=0;
-    for(int i=0;i<user_count;i++){
-        if (strcmp(username, registeredUser[i].username) == 0 && strcmp(password, registeredUser[i].password) == 0) {
-            printf("Login successful. Welcome, %s!\n", username);
-            found=1;
-            break;
-        }
-        if(found!=1) {
-            printf("Invalid username or password.\n");
-        }
+    FILE *file = fopen("UserInformation.txt", "r");
+    if (file == NULL) {
+        printf("\nNo users registered yet.\n");
+        return;
     }
 
+    while (fscanf(file, "Email: %s\nPassword: %s\n", user.email, user.password) != EOF) {
+        if (strcmp(email, user.email) == 0 && strcmp(password, user.password) == 0) {
+            found = 1;
+            break;
+        }
+    }
+    fclose(file);
+
+    if (found) {
+        printf("\nLogin successful. Welcome, %s!\n", user.username);
+    } else {
+        printf("\nInvalid email or password.\n");
+    }
 }
 
 int main() {
     int choice;
-
-    while (1) {
+    while(1) {
         //Navigation Menu
         printf("\n| --- Menu --- |\n");
         printf("1. Register\n");
@@ -68,19 +112,18 @@ int main() {
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1:
-                registerUser();
+            case 1: 
+                registerUser(); 
                 break;
-            case 2:
-                loginUser();
+            case 2: 
+                loginUser(); 
                 break;
-            case 3:
+            case 3: 
                 printf("Exiting the program. Goodbye!\n");
                 return 0;
-            default:
-                printf("Invalid choice! Please try again.\n");
+            default: 
+                printf("Invalid choice.\n");
         }
     }
-
     return 0;
 }
